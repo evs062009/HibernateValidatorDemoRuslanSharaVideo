5c875b63a54ddb075c444b38f6abf7c45bfbd073
yevhen.shevtsov <evs062009@gmail.com>
2019-11-28 19:50:13 +0200

    added @InitBinder method for trimming all string input

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Validation of input to jsp-form using:
 * Hibernate Validator (included in spring-boot-starter-validation);
 * JSP:
     - jstl (form creating);
-    - tomcat-embed-jasper (compile JSP).
+    - tomcat-embed-jasper (compile JSP);
+* @InitBinder method for trimming all string input.
--- src/main/java/com/controllers/CustomerController.java
+++ src/main/java/com/controllers/CustomerController.java
@@ -1,9 +1,12 @@
 package com.controllers;
 
 import com.domains.Customer;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,6 +15,14 @@ import javax.validation.Valid;
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+
+    //initdinder for trimming all input strings
+    //resolves validation issue for lasName whitespace input
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        StringTrimmerEditor trimmerEditor = new StringTrimmerEditor(true);
+        binder.registerCustomEditor(String.class, trimmerEditor);
+    }
 
     @RequestMapping("/showForm")
     public String showForm(Model model) {
