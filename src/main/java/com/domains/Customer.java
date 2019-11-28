bd85403fa6dd0e1105e7c160cb966e838aa564ca
yevhen.shevtsov <evs062009@gmail.com>
2019-11-28 11:33:59 +0200

    working without validation

--- src/main/java/com/domains/Customer.java
+++ src/main/java/com/domains/Customer.java
@@ -2,15 +2,15 @@ package com.domains;
 
 import lombok.Data;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+//import javax.validation.constraints.NotNull;
+//import javax.validation.constraints.Size;
 
 @Data
 public class Customer {
 
     private String firstName;
 
-    @NotNull(message = "is required")
-    @Size(min = 1, message = "is required")
+//    @NotNull(message = "is required")
+//    @Size(min = 1, message = "is required")
     private String lastName;
 }
