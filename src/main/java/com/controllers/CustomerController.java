62d9652597f100d987599a94c62a52fcc8a0ad12
yevhen.shevtsov <evs062009@gmail.com>
2019-11-28 16:44:30 +0200

    validation not working (conflict with Tomcat(?) )

--- src/main/java/com/controllers/CustomerController.java
+++ src/main/java/com/controllers/CustomerController.java
@@ -7,7 +7,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-//import javax.validation.Valid;
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/customer")
@@ -20,7 +20,7 @@ public class CustomerController {
     }
 
     @RequestMapping("/processForm")
-    public String processForm(/*@Valid*/ @ModelAttribute Customer customer, BindingResult validationResult) {
+    public String processForm(@Valid @ModelAttribute Customer customer, BindingResult validationResult) {
         if (validationResult.hasErrors()) {
             return "customer-form";
         }
